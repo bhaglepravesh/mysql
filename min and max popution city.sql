3abe180a8081e2f7ddd723a260fef76f06f3376a
Pravesh Kumar <84278273+bhaglepravesh@users.noreply.github.com>
2025-01-29 23:01:13 +0530

    Select the city from every state which has minimum and maximum population

---
+++ min and max popution city.sql
@@ -0,0 +1,29 @@
+use test;
+CREATE TABLE city_population (
+    state VARCHAR(50),
+    city VARCHAR(50),
+    population INT
+);
+
+-- Insert the data
+INSERT INTO city_population (state, city, population) VALUES ('haryana', 'ambala', 100);
+INSERT INTO city_population (state, city, population) VALUES ('haryana', 'panipat', 200);
+INSERT INTO city_population (state, city, population) VALUES ('haryana', 'gurgaon', 300);
+INSERT INTO city_population (state, city, population) VALUES ('punjab', 'amritsar', 150);
+INSERT INTO city_population (state, city, population) VALUES ('punjab', 'ludhiana', 400);
+INSERT INTO city_population (state, city, population) VALUES ('punjab', 'jalandhar', 250);
+INSERT INTO city_population (state, city, population) VALUES ('maharashtra', 'mumbai', 1000);
+INSERT INTO city_population (state, city, population) VALUES ('maharashtra', 'pune', 600);
+INSERT INTO city_population (state, city, population) VALUES ('maharashtra', 'nagpur', 300);
+INSERT INTO city_population (state, city, population) VALUES ('karnataka', 'bangalore', 900);
+INSERT INTO city_population (state, city, population) VALUES ('karnataka', 'mysore', 400);
+INSERT INTO city_population (state, city, population) VALUES ('karnataka', 'mangalore', 200);
+
+select *  from city_population; 
+
+with cte as (select *, row_number() over(partition by state order by population) as min_population,
+          row_number() over(partition by state order by population desc) as max_population
+from city_population)
+select state, min(case when min_population=1 then city end) as min_pop_city,
+              max(case when max_population=1 then city end) as max_pop_city
+              from cte group by state;
