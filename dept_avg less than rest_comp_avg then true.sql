c94977ed3ff68b9ef39e9d52b6480e7996bbca7a
Pravesh Kumar <84278273+bhaglepravesh@users.noreply.github.com>
2025-07-08 06:21:40 +0530

    Department avg salary vs remaining total department avg salary

--- dept_avg less than rest_comp_avg then true.sql
+++ dept_avg less than rest_comp_avg then true.sql
@@ -1,4 +1,6 @@
-use users;
+
+use world;
+-- drop table if exists emp;
 create table emp(
 emp_id int,
 emp_name varchar(20),
@@ -7,7 +9,7 @@ salary int,
 manager_id int,
 emp_age int);
 
-insert into emp
+insert into emp 
 values
 (1, 'Ankit', 100,10000, 4, 39);
 insert into emp
@@ -29,3 +31,25 @@ values (9, 'Mukesh',300,6000,6,51);
 insert into emp
 values (10, 'Rakesh',300,7000,6,50);
 select * from emp;
+
+-- department avg_salary is less than comp_avg_salary except that dept in comp_avg_salary
+
+with cte as(
+select department_id as dept_id,count(emp_id) as no_emp, avg(salary) as avg_sal,sum(salary) as total_dept_sal
+ from emp group by department_id)
+, cte1 as (select c1.dept_id, c1.avg_sal,sum(c2.total_dept_sal)/sum(c2.no_emp) as cmp_avg_sal from cte c1 
+join cte c2 on c1.dept_id !=c2.dept_id
+group by c1.dept_id,c1.avg_sal)
+select * from cte1 where avg_sal<cmp_avg_sal;
+
+
+select * from emp;
+
+SELECT department_id, AVG(salary) AS dept_avg_salary
+FROM emp e1
+GROUP BY department_id
+HAVING AVG(salary) < (
+    SELECT SUM(salary) / COUNT(emp_id)
+    FROM emp e2
+    WHERE e2.department_id != e1.department_id
+);
